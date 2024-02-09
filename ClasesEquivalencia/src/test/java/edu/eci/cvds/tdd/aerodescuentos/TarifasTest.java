affdd5588c334ebb3eabd275d3c6b4ec4e5a4c7b
Samuel Rojas <samu.02309@gmail.com>
2024-02-08 19:04:56 -0500

    Pruebas

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -51,5 +51,79 @@ public class TarifasTest {
             Assert.fail();
         }
     }
-    // TODO Complete with more test cases
+
+    // Pruebas de frontera
+
+     @Test
+    public void shouldCalculateFee_tafiraBase() {
+        CalculadorDescuentos calculador = new CalculadorDescuentos();
+        try{
+            double total = calculador.calculoTarifa(1, 10, 50);
+            Assert.assertEquals(1, total,0);
+        }
+        catch(Throwable e){
+            Assert.fail();
+        }
+    }
+
+     @Test
+    public void shouldthrowException_tafiraBase() {
+        CalculadorDescuentos calculador = new CalculadorDescuentos();
+        try{
+            double total = calculador.calculoTarifa(0, 10, 50);
+            Assert.assertEquals(1, total,0);
+        }
+        catch(Throwable e){
+            Assert.assertEquals(true, true);
+        }
+    }
+
+     @Test
+    public void shouldCalculateFee_diasAntelacion() {
+        CalculadorDescuentos calculador = new CalculadorDescuentos();
+        try{
+            double total = calculador.calculoTarifa(1, 0, 50);
+            Assert.assertEquals(1, total,0);
+        }
+        catch(Throwable e){
+            Assert.fail();
+        }
+    }
+
+     @Test
+    public void shouldthrowException_diasAntelacion() {
+        CalculadorDescuentos calculador = new CalculadorDescuentos();
+        try{
+            double total = calculador.calculoTarifa(100000, -1, 50);
+            Assert.assertEquals(1, total,0);
+        }
+        catch(Throwable e){
+            Assert.assertEquals(true, true);
+        }
+    }
+
+     @Test
+    public void shouldCalculateFee_edad() {
+        CalculadorDescuentos calculador = new CalculadorDescuentos();
+        try{
+            double total = calculador.calculoTarifa(10000, 10, 0);
+            Assert.assertEquals(9500, total,0);
+        }
+        catch(Throwable e){
+            Assert.fail();
+        }
+    }
+
+     @Test
+    public void shouldthrowException_edad() {
+        CalculadorDescuentos calculador = new CalculadorDescuentos();
+        try{
+            double total = calculador.calculoTarifa(100000, 10, -1);
+            Assert.assertEquals(1, total,0);
+        }
+        catch(Throwable e){
+             Assert.assertEquals(true, true);
+        }
+    }
+    
 }
